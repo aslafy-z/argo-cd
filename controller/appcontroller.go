792f03b0bd0a56c9d3c94855df66bab18a53b312
Zadkiel AHARONIAN <zadkiel_aharonian@carrefour.com>
2023-09-28 23:01:28 +0200

    fix

--- controller/appcontroller.go
+++ controller/appcontroller.go
@@ -1159,6 +1159,14 @@ func (ctrl *ApplicationController) processRequestedAppOperation(app *appv1.Appli
 				ctrl.requestAppRefresh(app.QualifiedName(), CompareWithLatest.Pointer(), &retryAfter)
 				return
 			} else {
+				if app.Spec.SyncPolicy.Retry.Refresh && app.Status.Sync.Revision != state.Operation.Sync.Revision {
+					logCtx.Infof("A new revision is available, refreshing and terminating app, was phase: %s, message: %s", state.Phase, state.Message)
+					ctrl.requestAppRefresh(app.QualifiedName(), CompareWithLatest.Pointer(), nil)
+					state.Phase = synccommon.OperationTerminating
+					state.Message = "Operation forced to terminate (new revision available)"
+					ctrl.setOperationState(app, state)
+					return
+				}
 				// retrying operation. remove previous failure time in app since it is used as a trigger
 				// that previous failed and operation should be retried
 				state.FinishedAt = nil
@@ -1166,12 +1174,6 @@ func (ctrl *ApplicationController) processRequestedAppOperation(app *appv1.Appli
 				// Get rid of sync results and null out previous operation completion time
 				state.SyncResult = nil
 			}
-		} else if state.Phase == synccommon.OperationRunning && app.Spec.SyncPolicy.Retry.Refresh && app.Status.Sync.Revision != state.Operation.Sync.Revision {
-			logCtx.Infof("A new revision is available, terminating app, was phase: %s, message: %s", state.Phase, state.Message)
-			state.Phase = synccommon.OperationTerminating
-			state.Message = "Operation forced to terminate (new revision available)"
-			ctrl.setOperationState(app, state)
-			return
 		} else {
 			logCtx.Infof("Resuming in-progress operation. phase: %s, message: %s", state.Phase, state.Message)
 		}
